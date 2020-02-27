ed2a29e4ef34d2078048b2edd4bf4d973902f9f9
Riku Yamamoto <ymmtr666@gmail.com>
2020-02-27 13:12:26 +0900

    React.Componentを継承させた形に変更

--- bookmanagement/src/BookCard.js
+++ bookmanagement/src/BookCard.js
@@ -2,34 +2,26 @@ import React from "react";
 import { ContentCard } from "./ContentCard";
 //import { Link } from "react-router-dom";
 
-const Books = ({ books }) => {
-  return (
-    books.map((book, index) => (
-      <ContentCard
-        pageName={book.title}
-        imgSrc={"thumbnail" in book ? (book.thumbnail) : "./img/noimage.png"}
-        discription={book.authors instanceof Array ? book.authors.join(" ") : book.authors}
-        key={book.isbn}
-      />
-    ))
+class Books extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      books: this.props.books
+    };
+  }
 
-  )
-};
+  render() {
+    return (
+      this.props.books.map((book, index) => (
+        <ContentCard
+          pageName={book.title}
+          imgSrc={"thumbnail" in book ? (book.thumbnail) : "./img/noimage.png"}
+          discription={book.authors instanceof Array ? book.authors.join(" ") : book.authors}
+          key={book.isbn}
+        />
+      ))
+    )
+  }
+}
 
 export default Books
-    /*
-const Books = ({ books }) => {
-return (
-<div className="inline-block">{
-books.map((book) => (
-<ContentCard
-pageName={book.title}
-imgSrc={"thumbnail" in book ? (book.thumbnail) : "./img/noimage.png"}
-discription={book.authors instanceof Array ? book.authors.join(" ") : book.authors}
-/>
-))
-}
-</div>
-)
-};
-*/
